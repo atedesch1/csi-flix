4985a6aff4aa107f187c4dfdd27e19d504fdbe2e
atedesch1 <alvarotedeschi@gmail.com>
2022-09-28 00:19:11 -0300

    add get by id and title

--- back/main.go
+++ back/main.go
@@ -23,5 +23,31 @@ func main() {
 		c.JSON(http.StatusOK, movies)
 	})
 
+	r.GET("/movies/:id", func(c *gin.Context) {
+		var movie database.Movie
+		id := c.Param("id")
+
+		tx := db.Db.Preload("Directors").Preload("Cast").Preload("Countries").Preload("Genres").First(&movie, id)
+
+		if tx.Error != nil {
+			c.JSON(http.StatusNotFound, nil)
+		} else {
+			c.JSON(http.StatusOK, movie)
+		}
+	})
+
+	r.GET("/movies/bytitle/:title", func(c *gin.Context) {
+		var movies []database.Movie
+		title := c.Param("title")
+
+		tx := db.Db.Preload("Directors").Preload("Cast").Preload("Countries").Preload("Genres").Where("Title LIKE ?", "%"+title+"%").Find(&movies)
+
+		if tx.Error != nil {
+			c.JSON(http.StatusInternalServerError, nil)
+		} else {
+			c.JSON(http.StatusOK, movies)
+		}
+	})
+
 	r.Run()
 }
