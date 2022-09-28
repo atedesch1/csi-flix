4f36b4cbd77f86baee9fb63927fe00357486d835
atedesch1 <alvarotedeschi@gmail.com>
2022-09-28 00:18:56 -0300

    add api simple get movies

--- back/main.go
+++ back/main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/atedesch1/csi-flix/cmd/database"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	db := database.SetupDB("postgresql://root:root@localhost:5432/core")
+	db.Migrate()
 	movies := database.ReadMovieCsv("assets/netflix_titles.csv")
 	db.PopulateDB(movies)
+
+	r := gin.Default()
+
+	r.GET("/movies", func(c *gin.Context) {
+		var movies []database.Movie
+
+		db.Db.Preload("Directors").Preload("Cast").Preload("Countries").Preload("Genres").Find(&movies)
+
+		c.JSON(http.StatusOK, movies)
+	})
+
+	r.Run()
 }
